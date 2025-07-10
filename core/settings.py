b6b67603f9493dcc46fb8b650583f97f27e2e261
Maxi González <gonzalezmaxi997@gmail.com>
2025-07-10 10:46:59 -0300

    Corrijo STATIC_URL para servir archivos estáticos

--- core/settings.py
+++ core/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = '/dashboard/static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Opcional (si usás archivos locales además de los de apps)
