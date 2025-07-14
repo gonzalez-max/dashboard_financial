6e0ba7690e4c06db67005bcfe08d83510afc07e6
Maxi González <gonzalezmaxi997@gmail.com>
2025-07-14 12:02:12 -0300

    cambiamos el nombre img src

--- core/settings.py
+++ core/settings.py
@@ -92,7 +92,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')# Opcional (si usás archivos locales además de los de apps)
 
-
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]# Para producción con WhiteNoise
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
